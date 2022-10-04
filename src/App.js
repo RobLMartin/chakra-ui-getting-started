31dedc664f584f14a4b122af3295b917afdfd1a8
Rob Martin <rmartin3@alumni.unca.edu>
2022-10-04 11:36:45 -0400

    added login form example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 function App() {
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background={formBackground} p={12} rounded={6}>
+        <Heading mb={6}>Log in</Heading>
+        <Input
+          placeholder="Rob@virtu.studio"
+          variant="filled"
+          mb={3}
+          type="email"
+        />
+        <Input placeholder="******" variant="filled" mb={6} type="password" />
+        <Button colorScheme="teal" mb={6}>
+          Sign in
+        </Button>
+        <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
+      </Flex>
+    </Flex>
   );
 }
 
